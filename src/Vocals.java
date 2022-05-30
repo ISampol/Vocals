27c2628f592bcbdf4920d6bf471bd0161f5c248b
Ismael <ismaelsampol@gmail.com>
2022-05-30 17:32:14 +0200

    Opcion 1: larga

--- src/Vocals.java
+++ src/Vocals.java
@@ -1,31 +1,29 @@
 import java.io.StringReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Vocals {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        String[] vocals = new String[]{"a", "e", "i", "o", "u"};
 
-//        String[] largoFrase = new String[5];
-        String cadena = "sdfsadfasf";
-        for (int i = 0; i < cadena.length(); i++){
-            char letra = cadena.charAt(i);
-            System.out.print(letra + " ");
+
+        System.out.println("Digues una frase");
+        String frase = sc.nextLine();
+
+        if (frase.contains("a") || frase.contains("e") || frase.contains("i") || frase.contains("o") || frase.contains("u")){
+            String fraseA = frase.replace("e", "a").replace("i", "a").replace("o","a").replace("u", "a");
+            String fraseE = frase.replace("a", "e").replace("i", "e").replace("o","e").replace("u", "e");
+            String fraseI = frase.replace("a", "i").replace("e", "i").replace("o","i").replace("u", "i");
+            String fraseU = frase.replace("a", "o").replace("e", "o").replace("i","o").replace("u", "o");
+            String fraseO = frase.replace("a", "u").replace("e", "u").replace("i","u").replace("o", "u");
+
+            System.out.println(fraseA);
+            System.out.println(fraseE);
+            System.out.println(fraseI);
+            System.out.println(fraseO);
+            System.out.println(fraseU);
         }
 
-
-
-//        for (int i = 0; i < largoFrase.length; i++) {
-//            System.out.println("Digues una frase");
-//            frase = sc.next();
-//            largoFrase[i] = frase;
-//        }
-//
-//        for (int i = 0; i < largoFrase.length; i++) {
-//            System.out.print(largoFrase[i] + " ");
-//        }
-
-        sc.close();
+            sc.close();
     }
 }
