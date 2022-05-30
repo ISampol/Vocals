fb89c90d0d50863f8bdb7a067f748fa37a3ab1b5
Ismael <ismaelsampol@gmail.com>
2022-05-30 17:56:45 +0200

    Opcion 2

--- src/Vocals.java
+++ src/Vocals.java
@@ -6,22 +6,15 @@ public class Vocals {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String[] vocals = new String[]{"a", "e", "i", "o", "u"};
 
         System.out.println("Digues una frase");
-        String frase = sc.nextLine();
+        String frase;
 
-        if (frase.contains("a") || frase.contains("e") || frase.contains("i") || frase.contains("o") || frase.contains("u")){
-            String fraseA = frase.replace("e", "a").replace("i", "a").replace("o","a").replace("u", "a");
-            String fraseE = frase.replace("a", "e").replace("i", "e").replace("o","e").replace("u", "e");
-            String fraseI = frase.replace("a", "i").replace("e", "i").replace("o","i").replace("u", "i");
-            String fraseU = frase.replace("a", "o").replace("e", "o").replace("i","o").replace("u", "o");
-            String fraseO = frase.replace("a", "u").replace("e", "u").replace("i","u").replace("o", "u");
-
-            System.out.println(fraseA);
-            System.out.println(fraseE);
-            System.out.println(fraseI);
-            System.out.println(fraseO);
-            System.out.println(fraseU);
+        for (int i = 0; i < vocals.length; i++) {
+            frase = sc.nextLine();
+            String fraseConvertida = frase.replace("a", vocals[i]).replace("e", vocals[i]).replace("i", vocals[i]).replace("o", vocals[i]).replace("u", vocals[i]);
+            System.out.println(fraseConvertida);
         }
 
             sc.close();
