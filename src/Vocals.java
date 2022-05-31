e963a98d75624ac57247cacb7775358b831cd2f3
Ismael <ismaelsampol@gmail.com>
2022-05-31 15:56:39 +0200

    Ejercicio acabado + comentarios

--- src/Vocals.java
+++ src/Vocals.java
@@ -6,14 +6,23 @@ public class Vocals {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        //STRING PER ESCRIURE LES LLETRES QUE SUSTITUIRAN A LES ALTRES
         String[] vocals = new String[]{"a", "e", "i", "o", "u"};
 
+        //ESCRIVIM UNA FRASE
         System.out.println("Digues una frase");
         String frase = sc.nextLine();
 
+        //CREEM UN FOR PER A QUE CADA VOLTA (ITERACIO) QUE FACI, CANVII LES LLETRES QUE DEMANEM PER LA LLETRA
+        //QUE HI HAGI A LA POSICIO DE L'STRING-ARRAY CREAT ABANS
         for (String vocal : vocals) {
-            String fraseConvertida = frase.replace("a", vocal).replace("e", vocal).replace("i", vocal).replace("o", vocal).replace("u", vocal);
-            System.out.println(fraseConvertida);
+
+            //Amb els parentesis '[]' especifiquem que volem reemplaçar totes les lletres que hi hagi dins d'ells,
+            //per per separat. Si escrivissim només "aeiou", buscaria dins el text exactament aeiou, cosa que no funcionaria
+            //pero al fer "[aeiou]", buscara la a e i o u per separat i les reemplaçara per el que indiquem.
+            //Aquest metode nomes funciona si volem canviar lletres
+            String novaFrase = frase.replaceAll("[aeiou]", vocal);
+            System.out.println(novaFrase);
         }
 
             sc.close();
